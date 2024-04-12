12125e6ec1f0ce99d01299f3048676a5b1c889fe
joelm18 <joelmedeiros.dev@gmail.com>
2024-04-12 02:57:49 -0300

    Meu primeiro comit

---
+++ main.py
@@ -0,0 +1,70 @@
+from register import Register
+import os
+
+clients = []
+
+print("Bem-vindo ao Super mercado!")
+print("-----------------------------------------------")
+print("Digite uma das opções a seguir:")
+print("1 - Cadastrar cliente")
+print("2 - Consultar saldo do cliente")
+print("3 - Atualizar saldo do cliente")
+print("4 - Ver lista de clientes cadastrados")
+print("5 - Remover um cliente")
+print("6 - Limpar tela")
+print("7 - Sair")
+print("-----------------------------------------------")
+
+while True:
+  option = int(input("Digite a opção desejada: "))
+  if option == 1:
+    name = input("Digite o nome do cliente: ")
+    age = int(input("Digite a idade do cliente: "))
+    balance = float(input("Digite o saldo do cliente: "))
+    cpf = int(input("Digite o CPF do cliente: "))
+    client = Register(name, age, balance, cpf)
+    clients.append(client)
+
+    #Salva o cliente no arquivo ao invés de escolher a opção de salvar
+    with open("clients.txt", "a") as file:
+      file.write(f" Nome:\t{name}\n Idade:\t{age}\n Saldo:\t{balance}\n CPF:\t{cpf}\n")
+    
+    print("Cliente cadastrado com sucesso!")
+  elif option == 2:
+    cpf = int(input("Digite o CPF do cliente: "))
+    for client in clients:
+      if client.cpf == cpf:
+        print(f"Saldo do cliente {client.nome}: R${client.saldo}")
+        break
+    else:
+      print("Cliente não encontrado!")
+  elif option == 3:
+    cpf = int(input("Digite o CPF do cliente: "))
+    new_balance = int(input("Digite o novo saldo do cliente: "))
+    for client in clients:
+      if client.cpf == cpf:
+        client.update_balance(new_balance)
+        print("Saldo atualizado com sucesso!")
+        break
+    else:
+      print("Cliente não encontrado!")
+  elif option == 4:
+    with open("clients.txt", "r") as file:
+      for line in file:
+        print(line.strip())
+  elif option == 5:
+    cpf = int(input("Digite o CPF do cliente: "))
+    for client in clients:
+      if client.cpf == cpf:
+        clients.remove(client)
+        print("Cliente removido com sucesso!")
+        break
+    else:
+      print("Cliente não encontrado!")
+  elif option == 6:
+    os.system('cls' if os.name == 'nt' else 'clear')
+  elif option == 7:
+    print("Obrigado por usar o Super mercado!")
+    break
+  else:
+    print("Opção inválida!")
---
+++ register.py
@@ -0,0 +1,24 @@
+import regex
+
+class Register:
+
+  #Método construtor da classe
+  def __init__(self, name, age, balance, cpf):
+    self.nome = name
+    self.idade = age
+    self.saldo = balance
+    self.cpf = cpf
+  # Cadastra o usuário
+  def register(self, name, age, balance, cpf):
+    self.nome = name
+    self.idade = age
+    self.saldo = balance
+    self.cpf = cpf
+    
+  # Consulta o saldo da galera
+  def get_balance(self, balance):
+    self.saldo = balance
+    return balance
+
+  def update_balance(self, new_balance):
+    self.saldo = new_balance
